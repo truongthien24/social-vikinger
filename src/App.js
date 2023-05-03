8e4060ccd3cf12358608579af2de945d36faca8c
truongthien24 <truongthien2411@gmail.com>
2023-05-03 11:34:16 +0700

    update code

--- src/App.js
+++ src/App.js
@@ -26,73 +26,6 @@ function App() {
 
     const {pathname} = window.location;
 
-    // useEffect(() => {
-    //   const script = document.createElement("script");
-    //   const script2 = document.createElement("script");
-    //   const script3 = document.createElement("script");
-    //   const script4 = document.createElement("script");
-    //   const script5 = document.createElement("script");
-    //   const script6 = document.createElement("script");
-    //   const script7 = document.createElement("script");
-    //   const script8 = document.createElement("script");
-    //   const script9 = document.createElement("script");
-    //   const script10 = document.createElement("script");
-    //   const script11 = document.createElement("script");
-    //   const script12 = document.createElement("script");
-    //   const script13 = document.createElement("script");
-    //   const script14 = document.createElement("script");
-    //   const script15 = document.createElement("script");
-    //   const script16 = document.createElement("script");
-    //   script.src = "/js/utils/app.js";
-    //   script2.src = "/js/vendor/xm_plugins.min.js";
-    //   script3.src = "/js/form/form.utils.js";
-    //   script4.src = "/js/landing/landing.tabs.js";
-    //   script5.src = "/js/utils/svg-loader.js";
-    //   script6.src = "/js/utils/page-loader.js";
-    //   script7.src = "/js/vendor/simplebar.min.js";
-    //   script8.src = "/js/utils/liquidify.js";
-    //   script9.src = "/js/vendor/tiny-slider.min.js";
-    //   script10.src = "/js/vendor/Chart.bundle.min.js";
-    //   script11.src = "/js/global/global.hexagons.js";
-    //   script12.src = "/js/global/global.tooltips.js";
-    //   script13.src = "/js/global/global.charts.js";
-    //   script14.src = "/js/header/header.js";
-    //   script15.src = "/js/sidebar/sidebar.js";
-    //   script16.src = "/js/content/content.js";
-    //   script.async = true;
-    //   script2.async = true;
-    //   script3.async = true;
-    //   script4.async = true;
-    //   script5.async = true;
-    //   script6.async = true;
-    //   script7.async = true;
-    //   script8.async = true;
-    //   script9.async = true;
-    //   script10.async = true;
-    //   script11.async = true;
-    //   script12.async = true;
-    //   script13.async = true;
-    //   script14.async = true;
-    //   script15.async = true;
-    //   script16.async = true;
-    //   document.body.appendChild(script);
-    //   document.body.appendChild(script2);
-    //   document.body.appendChild(script3);
-    //   document.body.appendChild(script4);
-    //   document.body.appendChild(script5);
-    //   document.body.appendChild(script6);
-    //   document.body.appendChild(script7);
-    //   document.body.appendChild(script8);
-    //   document.body.appendChild(script9);
-    //   document.body.appendChild(script10);
-    //   document.body.appendChild(script11);
-    //   document.body.appendChild(script12);
-    //   document.body.appendChild(script13);
-    //   document.body.appendChild(script14);
-    //   document.body.appendChild(script15);
-    //   document.body.appendChild(script16);
-    //  },[])
-
      useEffect(()=> {
         let script = document.createElement("script");
         let script2 = document.createElement("script");
@@ -181,24 +114,6 @@ function App() {
         {/* <Loading/> */}
         <div>{routes}</div>
         <Toaster position='top-right'/>
-        {/* <Helmet >
-          <script src="/js/utils/app.js"></script>
-          <script src="/js/vendor/xm_plugins.min.js"></script>
-          <script src="/js/form/form.utils.js"></script>
-          <script src="/js/landing/landing.tabs.js"></script>
-          <script src="/js/utils/svg-loader.js"></script>
-          <script src="/js/utils/page-loader.js"></script>
-          <script src="/js/vendor/simplebar.min.js"></script>
-          <script src="/js/utils/liquidify.js"></script>
-          <script src="/js/vendor/tiny-slider.min.js"></script>
-          <script src="/js/vendor/Chart.bundle.min.js"></script>
-          <script src="/js/global/global.hexagons.js"></script>
-          <script src="/js/global/global.tooltips.js"></script>
-          <script src="/js/global/global.charts.js"></script>
-          <script src="/js/header/header.js"></script>
-          <script src="/js/sidebar/sidebar.js"></script>
-          <script src="/js/content/content.js"></script>
-        </Helmet> */}
       </>
     );
 }
